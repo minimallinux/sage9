d6c544952209c930f6f483fa973545226412e525
minimallinux <anthonymcgowan1@gmail.com>
2018-03-05 07:34:10 +0000

    Update

--- app/setup.php
+++ app/setup.php
@@ -185,24 +185,7 @@ function my_acf_settings_dir( $dir ) {
 // 4. Include ACF
 include_once( get_stylesheet_directory() . '/acf/acf.php' );
 });
-// 1. customize CMC path
-add_filter('cmc/settings/path', __NAMESPACE__. '\my_cmc_settings_path');
-function my_cmc_settings_path( $path ) {
- // update path
- $path = get_stylesheet_directory() . '/cmc/';
- // return
- return $path; 
-}
-// 2. customize CMC dir
-add_filter('cmc/settings/dir', __NAMESPACE__ . '\my_cmc_settings_dir');
-function my_cmc_settings_dir( $dir ) {
- // update path
- $dir = get_stylesheet_directory_uri() . '/cmc/';
- // return
- return $dir; 
-}
-// 4. Include CMC
-include_once( get_stylesheet_directory() . '/cmc/custom-menu-class.php' );
+
 // 1. customize Woocommerce path
 add_filter('woocommerce/settings/path', __NAMESPACE__. '\my_woocommerce_settings_path');
 function my_woocommerce_settings_path( $path ) {
